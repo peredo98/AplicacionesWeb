df7a3cf46107e2fbe9f6539b5bd67200f5b90119
Nayra1316 <RafaelDM@users.noreply.github.com>
2020-05-04 18:28:29 -0500

    Modules and components
    routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,30 +6,16 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AboutComponent } from './about/about.component';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
-import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { PoliciesComponent } from './policies/policies.component';
 import { SignupComponent } from './signup/signup.component';
 import { LoginFormComponent } from './login-form/login-form.component';
 import { RegisterFormComponent } from './register-form/register-form.component';
-import { HomeCarouselComponent } from './home-carousel/home-carousel.component';
 import { HeaderNavbarComponent } from './header-navbar/header-navbar.component';
-import { ListaEncuestasAdminComponent } from './lista-encuestas-admin/lista-encuestas-admin.component';
-import { ListaEncuestasUsuarioComponent } from './lista-encuestas-usuario/lista-encuestas-usuario.component';
-import { EstadisticasAdminComponent } from './estadisticas-admin/estadisticas-admin.component';
-import { EstadisticasUsuarioComponent } from './estadisticas-usuario/estadisticas-usuario.component';
-import { CrearEncuestasComponent } from './crear-encuestas/crear-encuestas.component';
-import { EncuestasAdminComponent } from './encuestas-admin/encuestas-admin.component';
-import { EncuestasUsuarioComponent } from './encuestas-usuario/encuestas-usuario.component';
-import { GraficasAdminComponent } from './graficas-admin/graficas-admin.component';
-import { GraficasUsuarioComponent } from './graficas-usuario/graficas-usuario.component';
-import { FechasAdminComponent } from './fechas-admin/fechas-admin.component';
-import { FechasUsuarioComponent } from './fechas-usuario/fechas-usuario.component';
-import { CrearPreguntaComponent } from './crear-pregunta/crear-pregunta.component';
-import { ContestarEncuestaComponent } from './contestar-encuesta/contestar-encuesta.component';
-import { ContestarPreguntaComponent } from './contestar-pregunta/contestar-pregunta.component';
-import { AppRoutingModule, routingComponents } from './app-routing.module';
+import { AppRoutingModule} from './app-routing.module';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { UsuarioModule } from './modules/usuario/usuario.module';
+import { AdministradorModule } from './modules/administrador/administrador.module';
 
 @NgModule({
   declarations: [
@@ -37,35 +23,21 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
     AboutComponent,
     FooterComponent,
     HeaderComponent,
-    HomeComponent,
     LoginComponent,
     PoliciesComponent,
     SignupComponent,
     LoginFormComponent,
     RegisterFormComponent,
-    HomeCarouselComponent,
     HeaderNavbarComponent,
-    ListaEncuestasAdminComponent,
-    ListaEncuestasUsuarioComponent,
-    EstadisticasAdminComponent,
-    EstadisticasUsuarioComponent,
-    CrearEncuestasComponent,
-    EncuestasAdminComponent,
-    EncuestasUsuarioComponent,
-    GraficasAdminComponent,
-    GraficasUsuarioComponent,
-    FechasAdminComponent,
-    FechasUsuarioComponent,
-    CrearPreguntaComponent,
-    ContestarEncuestaComponent,
-    ContestarPreguntaComponent,
-    routingComponents,
     PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AdministradorModule,
+    UsuarioModule,
+  
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/modules/administrador/administrador.module.ts
+++ src/app/modules/administrador/administrador.module.ts
@@ -1,12 +1,36 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-
+import {AdministradorRoutingModule} from './administrador-routing.module';
+import { CrearEncuestasComponent } from './crear-encuestas/crear-encuestas.component';
+import { CrearPreguntaComponent } from './crear-pregunta/crear-pregunta.component';
+import { EncuestasAdminComponent } from './encuestas-admin/encuestas-admin.component';
+import { EstadisticasAdminComponent } from './estadisticas-admin/estadisticas-admin.component';
+import { FechasAdminComponent } from './fechas-admin/fechas-admin.component';
+import { FooterComponent } from './footer/footer.component';
+import { GraficasAdminComponent } from './graficas-admin/graficas-admin.component';
+import { HeaderComponent } from './header/header.component';
+import { HeaderNavbarComponent } from './header-navbar/header-navbar.component';
+import { ListaEncuestasAdminComponent } from './lista-encuestas-admin/lista-encuestas-admin.component';
+import { HomeComponent } from './home/home.component';
+import { HomeCarouselComponent } from './home-carousel/home-carousel.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [CrearEncuestasComponent, 
+                CrearPreguntaComponent, 
+                EncuestasAdminComponent, 
+                EstadisticasAdminComponent, 
+                FechasAdminComponent, 
+                FooterComponent, 
+                GraficasAdminComponent, 
+                HeaderComponent, 
+                HeaderNavbarComponent, 
+                HomeComponent,
+                HomeCarouselComponent,
+                ListaEncuestasAdminComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    AdministradorRoutingModule,
   ]
 })
 export class AdministradorModule { }
